6b0241b42bda597688b6b3fd8d4f4e58fd4954b1
hsggj002 <499864887@qq.com>
2022-03-07 10:40:58 +0800

    Update Alert.py
    兼容k8s告警格式

--- Alert.py
+++ Alert.py
@@ -13,39 +13,49 @@ def parse_time(*args):
         times.append(eta)
     return times
 
-def alert(types,levels,times,instances):
+def alert(types,levels,times,ins,instance):
     params = json.dumps({
         "msgtype": "text",
         "text":
             {
-                "content": "**********告警通知**********\n告警类型: {0}\n告警级别: {1}\n故障时间: {2}\n故障实例: {3}".format(types,levels,times[0],instances)
+                "content": "**********告警通知**********\n告警类型: {0}\n告警级别: {1}\n故障时间: {2}\n{3}: {4}".format(types,levels,times[0],ins,instance)
             }
         })
 
     return params
 
-def recive(types,levels,times,instances):
+def recive(types,levels,times,ins,instance):
     params = json.dumps({
         "msgtype": "text",
         "text":
             {
-                "content": "**********恢复通知**********\n告警类型: {0}\n告警级别: {1}\n故障时间: {2}\n\n恢复时间: {3}\n故障实例: {4}".format(types,levels,times[0],times[1],instances)
+                "content": "**********恢复通知**********\n告警类型: {0}\n告警级别: {1}\n故障时间: {2}\n\n恢复时间: {3}\n{4}: {5}".format(types,levels,times[0],times[1],ins,instance)
             }
         })
 
     return params
 
-def webhook_url(params):
+def webhook_url(params,url_key):
     headers = {"Content-type": "application/json"}
     “”“
     *****重要*****
     ”“”
-    url = "使用你上面保存的webhook地址"
+    url = "{}".format(url_key)
     r = requests.post(url,params,headers)
 
-def send_alert(json_re):
+def send_alert(json_re,url_key):
     for i in json_re['alerts']:
         if i['status'] == 'firing':
-            webhook_url(alert(i['labels']['alertname'],i['labels']['severity'],parse_time(i['startsAt']),i['labels']['instance']))
+            if "instance" in i['labels']:
+                webhook_url(alert(i['labels']['alertname'],i['labels']['severity'],parse_time(i['startsAt']),'故障实例',i['labels']['instance']),url_key)
+            elif "namespace" in i['labels']:
+                webhook_url(alert(i['labels']['alertname'],i['labels']['severity'],parse_time(i['startsAt']),'名称空间',i['labels']['namespace']),url_key)
+            elif "Watchdog" in i['labels']['alertname']:
+                webhook_url(alert(i['labels']['alertname'],'0','0','0','0'),url_key)
         elif i['status'] == 'resolved':
-            webhook_url(recive(i['labels']['alertname'],i['labels']['severity'],parse_time(i['startsAt'],i['endsAt']),i['labels']['instance']))
+            if "instance" in i['labels']:
+                webhook_url(recive(i['labels']['alertname'],i['labels']['severity'],parse_time(i['startsAt'],i['endsAt']),'故障实例',i['labels']['instance']),url_key)
+            elif "namespace" in i['labels']:
+                webhook_url(recive(i['labels']['alertname'],i['labels']['severity'],parse_time(i['startsAt'],i['endsAt']),'名称空间',i['labels']['namespace']),url_key)
+            elif "Watchdog" in i['labels']['alertname']:
+                webhook_url(alert(i['labels']['alertname'],'0','0','0','0'),url_key)
