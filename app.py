eccdca89c862c7413f8c92b31dcda53dade8b7a3
hsggj002 <499864887@qq.com>
2022-03-07 10:52:07 +0800

    Update app.py
    将urlkey转为外部传入方式

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, request
+from flask_json import as_json
+from flask import jsonify
+import sys
 import Alert
 import requests
 import json
@@ -10,8 +13,17 @@ def alert_data():
     data = request.get_data()
     json_re = json.loads(data)
     json.dumps(json_re)
-    Alert.send_alert(json_re)
+    Alert.send_alert(json_re, args.key)
     return json_re
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int, help="The service port")
+    parser.add_argument("-k", "--key", type=str, help="The webhook url key")
+    args = parser.parse_args()
+
+    if not args.port or not args.key:
+        parser.print_help()
+        sys.exit(0)
+        
+    app.run(host="0.0.0.0", port=args.port)
